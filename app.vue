8b0528645bfd2ce60b4028fd33894d29cb792a20
Ryan Garrett <ryan@thepinkhacker.com>
2023-12-14 23:32:12 -0500

    Setup Firebase

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>Hi there</p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+div {
+  background-color: red;
+
+  p {
+    color: black;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    preset: "firebase",
+    firebase: {
+      nodeVersion: "18",
+      gen: 1,
+      region: "us-central1",
+    },
+  },
 })
